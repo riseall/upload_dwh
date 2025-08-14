cb3b2dfbd9d343e372e442c1d4deec1b4bcd00ce
Rizal Nugroho <rizalnug2@gmail.com>
2025-08-14 11:26:15 +0700

    feat: add login fieature and style

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\MstSAMController;
 use App\Http\Controllers\SellOutAMEController;
 use App\Http\Controllers\SellOutKMEController;
 use App\Http\Controllers\TopMarketingController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,45 +25,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('wel');
+Route::middleware('auth')->group(function () {
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+    Route::get('/dashboard', function () {
+        return view('welcome');
+    })->name('wel');
 
-Route::middleware('auth')->group(function () {});
-// Route Master Cabang Distributor
-Route::resource('mst_cbg_dist', MstCbgDistController::class)->except('show', 'create', 'store');
-// Route Master Cabang Phapros
-Route::resource('mst_cbg_ph', MstCbgPHController::class)->except('show', 'create', 'store');
-// Route Master Customer Distributor Phapros
-Route::get('/mst_cust_dist/data', [MstCustDistController::class, 'getData'])->name('mst_cust_dist.data');
-Route::resource('mst_cust_dist', MstCustDistController::class)->only('index', 'store');
-// Route Data Target Operasional
-Route::get('/top_marketing/data', [TopMarketingController::class, 'getData'])->name('top_marketing.data');
-Route::resource('top_marketing', TopMarketingController::class)->only('index', 'store');
-// Route Master Coverage AM
-Route::get('/mst_am/data', [MstAMController::class, 'getData'])->name('mst_am.data');
-Route::resource('mst_am', MstAMController::class)->only('index', 'store');
-// Route Master Coverage GM
-Route::get('/mst_gm/data', [MstGMController::class, 'getData'])->name('mst_gm.data');
-Route::resource('mst_gm', MstGMController::class)->only('index', 'store');
-// Route Master Coverage MR
-Route::get('/mst_mr/data', [MstMRController::class, 'getData'])->name('mst_mr.data');
-Route::resource('mst_mr', MstMRController::class)->only('index', 'store');
-// Route Master Coverage RM
-Route::get('/mst_rm/data', [MstRMController::class, 'getData'])->name('mst_rm.data');
-Route::resource('mst_rm', MstRMController::class)->only('index', 'store');
-// Route Master Coverage SAM
-Route::get('/mst_sam/data', [MstSAMController::class, 'getData'])->name('mst_sam.data');
-Route::resource('mst_sam', MstSAMController::class)->only('index', 'store');
-// Route Selling Out AME
-Route::post('/sell_out_ame/data', [SellOutAMEController::class, 'getData'])->name('sell_out_ame.data');
-Route::resource('sell_out_ame', SellOutAMEController::class)->only('index', 'store');
-// Route Selling Out KME
-Route::post('/sell_out_kme/data', [SellOutKMEController::class, 'getData'])->name('sell_out_kme.data');
-Route::resource('sell_out_kme', SellOutKMEController::class)->only('index', 'store');
-
+    Route::resource('users', UserController::class)->except('show', 'destroy');
+    // Route Master Cabang Distributor
+    Route::resource('mst_cbg_dist', MstCbgDistController::class)->except('show', 'create', 'store');
+    // Route Master Cabang Phapros
+    Route::resource('mst_cbg_ph', MstCbgPHController::class)->except('show', 'create', 'store');
+    // Route Master Customer Distributor Phapros
+    Route::get('/mst_cust_dist/data', [MstCustDistController::class, 'getData'])->name('mst_cust_dist.data');
+    Route::resource('mst_cust_dist', MstCustDistController::class)->only('index', 'store');
+    // Route Data Target Operasional
+    Route::get('/top_marketing/data', [TopMarketingController::class, 'getData'])->name('top_marketing.data');
+    Route::resource('top_marketing', TopMarketingController::class)->only('index', 'store');
+    // Route Master Coverage AM
+    Route::get('/mst_am/data', [MstAMController::class, 'getData'])->name('mst_am.data');
+    Route::resource('mst_am', MstAMController::class)->only('index', 'store');
+    // Route Master Coverage GM
+    Route::get('/mst_gm/data', [MstGMController::class, 'getData'])->name('mst_gm.data');
+    Route::resource('mst_gm', MstGMController::class)->only('index', 'store');
+    // Route Master Coverage MR
+    Route::get('/mst_mr/data', [MstMRController::class, 'getData'])->name('mst_mr.data');
+    Route::resource('mst_mr', MstMRController::class)->only('index', 'store');
+    // Route Master Coverage RM
+    Route::get('/mst_rm/data', [MstRMController::class, 'getData'])->name('mst_rm.data');
+    Route::resource('mst_rm', MstRMController::class)->only('index', 'store');
+    // Route Master Coverage SAM
+    Route::get('/mst_sam/data', [MstSAMController::class, 'getData'])->name('mst_sam.data');
+    Route::resource('mst_sam', MstSAMController::class)->only('index', 'store');
+    // Route Selling Out AME
+    Route::post('/sell_out_ame/data', [SellOutAMEController::class, 'getData'])->name('sell_out_ame.data');
+    Route::resource('sell_out_ame', SellOutAMEController::class)->only('index', 'store');
+    // Route Selling Out KME
+    Route::post('/sell_out_kme/data', [SellOutKMEController::class, 'getData'])->name('sell_out_kme.data');
+    Route::resource('sell_out_kme', SellOutKMEController::class)->only('index', 'store');
+});
 require __DIR__ . '/auth.php';
