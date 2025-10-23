66df5987a75b4e4fd72efb6af674003d0eaa24a2
Rizal Nugroho <rizalnug2@gmail.com>
2025-10-23 13:43:53 +0700

    feat: perubahan kecil

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app', ['title' => 'Dashboard'])
 @section('content')
-    <h2 class="card-label"> <strong>Selamat Datang</strong>, {{ Auth::user()->name }}!
+    <h2 class="card-label"> Selamat Datang, <strong> {{ Auth::user()->name }}!</strong>
         <span class="d-block text-muted pt-2 font-size-lg">Ini adalah halaman utama</span>
     </h2>
 @endsection
