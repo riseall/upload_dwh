4f00dc0c0803bfe57b5e0889c9559682a0595037
Rizal Nugroho <rizalnug2@gmail.com>
2025-10-28 15:09:47 +0700

    fix: error table model count

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,31 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\MstAM;
-use App\Models\MstCbgDist;
-use App\Models\MstCbgPH;
-use App\Models\MstCustDist;
-use App\Models\MstGM;
-use App\Models\MstMR;
-use App\Models\MstRM;
-use App\Models\MstSAM;
-use App\Models\TopMarketing;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $cbg_dist = MstCbgDist::all()->Count();
-        $cbg_ph = MstCbgPH::all()->Count();
-        $cust_dist = MstCustDist::all()->Count();
-        $cov_am = MstAM::all()->Count();
-        $cov_gm = MstGM::all()->Count();
-        $cov_mr = MstMR::all()->Count();
-        $cov_rm = MstRM::all()->Count();
-        $cov_sam = MstSAM::all()->Count();
-        $top = DB::table('top_marketing')->count();;
+        $cbg_dist = DB::table('mst_cbg_dist')->Count();
+        $cbg_ph = DB::table('mst_cbg_ph')->Count();
+        $cust_dist = DB::table('mst_cust_dist')->count();
+        $cov_am = DB::table('mst_am')->Count();
+        $cov_gm = DB::table('mst_gm')->Count();
+        $cov_mr = DB::table('mst_mr')->Count();
+        $cov_rm = DB::table('mst_rm')->Count();
+        $cov_sam = DB::table('mst_sam')->Count();
+        $top = DB::table('top_marketing')->count();
 
         return view('welcome', compact('cbg_dist', 'cbg_ph', 'cust_dist', 'cov_am', 'cov_gm', 'cov_mr', 'cov_rm', 'cov_sam', 'top'));
     }
