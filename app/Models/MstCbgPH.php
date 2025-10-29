81db2a5988f2eb1a6edf48b948928d481d25105c
Rizal Nugroho <rizalnug2@gmail.com>
2025-10-29 08:42:06 +0700

    feat: soft delete implementations

--- app/Models/MstCbgPH.php
+++ app/Models/MstCbgPH.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MstCbgPH extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'mst_cbg_ph';
 
